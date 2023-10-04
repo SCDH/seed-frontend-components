b014af6f6eb4a0308bfcbf2b76732a8807c7478d
Christian Lück <christian.lueck@uni-muenster.de>
2023-10-04 09:43:45 +0200

    intermediate class for LitElements that access the transformer API

---
+++ src/seed/components/transform-rest.ts
@@ -0,0 +1,18 @@
+import { LitElement } from 'lit'
+import { property } from 'lit/decorators.js'
+import { Configuration } from 'seed-xml-transformer-ts-client/configuration.ts'
+//import { DefaultApiFactory } from 'seed-xml-transformer-ts-client/api.ts'
+
+
+export abstract class TransformRESTElement extends LitElement {
+
+    @property({ type: String })
+    apiBase: string = "";
+
+    protected getConfiguration(): Configuration {
+	return new Configuration({
+	    basePath: this.apiBase
+	});
+    }
+
+}
