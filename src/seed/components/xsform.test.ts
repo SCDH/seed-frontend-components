3dd76d0c2ba4c3d43565b5f3a4658e615b2857b6
Christian Lück <christian.lueck@uni-muenster.de>
2023-10-12 12:55:47 +0100

    function for registering default value converters

--- src/seed/components/xsform.test.ts
+++ src/seed/components/xsform.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest';
-import { DefaultValueConverter, XSFormFieldFactory } from './xsform';
+import { DefaultValueConverter, XSFormFieldFactory, registerDefaultValueConverters } from './xsform';
 import { StringDefaultValueConverter, PassDefaultValueConverter, BooleanDefaultValueConverter } from './xsform';
 
 
@@ -24,8 +24,35 @@ test("boolean default value converter", () => {
 
 
 test("default value converter registry", () => {
+
+    XSFormFieldFactory.resetDefaultValueConverters();
+
     XSFormFieldFactory.addDefaultValueConverter("xs:string", "", new StringDefaultValueConverter());
     XSFormFieldFactory.addDefaultValueConverter("xs:boolean", "", new BooleanDefaultValueConverter());
+
+    expect(XSFormFieldFactory.getDefaultValueConverter("xs:string", "") instanceof StringDefaultValueConverter).toBe(true);
+    expect(XSFormFieldFactory.getDefaultValueConverter("xs:string", "").convert("'hello'")).toBe("hello");
+
+    expect(XSFormFieldFactory.getDefaultValueConverter("xs:boolean", "") instanceof BooleanDefaultValueConverter).toBe(true);
+    expect(XSFormFieldFactory.getDefaultValueConverter("xs:boolean", "").convert("true()")).toBe("true");
+
+})
+
+test("default value converter registry has fallback", () => {
+
+    XSFormFieldFactory.resetDefaultValueConverters();
+
+    expect(XSFormFieldFactory.getDefaultValueConverter("xs:string", "") instanceof PassDefaultValueConverter).toBe(true);
+    expect(XSFormFieldFactory.getDefaultValueConverter("xs:string", "").convert("'hello'")).toBe("'hello'");
+
+
+})
+
+
+test("default value converter registry filled by registerDefaultValueConverters()", () => {
+
+    XSFormFieldFactory.resetDefaultValueConverters();
+    registerDefaultValueConverters();
 
     expect(XSFormFieldFactory.getDefaultValueConverter("xs:string", "") instanceof StringDefaultValueConverter).toBe(true);
     expect(XSFormFieldFactory.getDefaultValueConverter("xs:string", "").convert("'hello'")).toBe("hello");
--- src/seed/components/xsform.ts
+++ src/seed/components/xsform.ts
@@ -1,29 +1,27 @@
+/*
+ * An interface for default value converters. Default values originate
+ * from xsl:param/@select attribtue values in XSLT
+ * stylesheets. Generally the values need to be converted. E.g. for an
+ * xs:string value, the quote must be removed.
+ */
 export interface DefaultValueConverter {
 
+    /**
+     * Convert the value from xsl:param/@select to HTML form data.
+     *
+     * @param value {string} - the value given in xsl:param/@select
+     * @return the value suitable for the HTML form and suitable for
+     * passing it to the REST interface of the SEED XML Transformer.
+     */
     convert(value: string): string;
 
 }
 
-export class XSFormFieldFactory {
 
-    static defaultValueConverters: { [key: string]: DefaultValueConverter } = {};
-
-    static addDefaultValueConverter(itemType:string, occurrenceIndicator: string, converter: DefaultValueConverter) {
-	const key: string = XSFormFieldFactory.mkKey(itemType, occurrenceIndicator);
-	XSFormFieldFactory.defaultValueConverters[key] = converter;
-    }
-    
-    static getDefaultValueConverter(itemType: string, occurrenceIndicator: string) {
-	const key: string = XSFormFieldFactory.mkKey(itemType, occurrenceIndicator);
-	return XSFormFieldFactory.defaultValueConverters?.[key];
-    }
-
-    protected static mkKey(itemType: string, occurrenceIndicator: string): string {
-	return itemType + occurrenceIndicator;
-    }
-
-}
-
+/**
+ * A converter from default values of type xs:string. The quotes are
+ * removed.
+ */
 export class StringDefaultValueConverter implements DefaultValueConverter {
 
     convert(value: string): string {
@@ -37,6 +35,10 @@ export class StringDefaultValueConverter implements DefaultValueConverter {
 
 }
 
+/**
+ * A converter from default values of type xs:boolean. The parens are
+ * removed.
+ */
 export class BooleanDefaultValueConverter implements DefaultValueConverter {
 
     convert(value: string): string {
@@ -54,10 +56,83 @@ export class BooleanDefaultValueConverter implements DefaultValueConverter {
 
 }
 
+/**
+ * This converter for XSLT default parameter values leaves the value
+ * untouched.
+ */
 export class PassDefaultValueConverter implements DefaultValueConverter {
 
     convert(value: string): string {
 	return value;
     }
 
+}
+
+/**
+ * A registry for converters that follows the factory pattern for
+ * instantiating converters. A converter for default values for a
+ * sequence of xs:string values can be instantiated by calling the
+ * static method
+ * {XSFormFieldFactory.getDefaultValueConverter("xs:string", "*")}.
+ *
+ * For registering new use the static method
+ * {XSFormFieldFactory.addDefaultValueConverter(your-converter)}.
+ */
+export class XSFormFieldFactory {
+
+    /**
+     * This is a static property (class property)!
+     */
+    private static defaultValueConverters: { [key: string]: DefaultValueConverter } = {};
+
+    static fallbackDefaultValueConverter: DefaultValueConverter = new PassDefaultValueConverter();
+
+
+    /**
+     * A static method for registering converters for default values.
+     */
+    static addDefaultValueConverter(itemType:string, occurrenceIndicator: string, converter: DefaultValueConverter) {
+	const key: string = XSFormFieldFactory.mkKey(itemType, occurrenceIndicator);
+	XSFormFieldFactory.defaultValueConverters[key] = converter;
+    }
+
+    /*
+     * A static method for getting an instance of a converter for
+     * default values from a pair of xs-type and occurrence indicator.
+     *
+     * @param itemType {string} - the xs item type, e.g. "xs:string" or "xs:boolean"
+     * @param occurrenceIndicator {string} - the sequence indicator, e.g. empty string or "*"
+     * @returns The registered converter or a fallback converter
+     */
+    static getDefaultValueConverter(itemType: string, occurrenceIndicator: string) {
+	const key: string = XSFormFieldFactory.mkKey(itemType, occurrenceIndicator);
+	if (key in XSFormFieldFactory.defaultValueConverters) {
+	    return XSFormFieldFactory.defaultValueConverters[key];
+	} else if ("all"+occurrenceIndicator in XSFormFieldFactory.defaultValueConverters) {
+	    return XSFormFieldFactory.defaultValueConverters["all" + occurrenceIndicator];
+	} else {
+	    return XSFormFieldFactory.fallbackDefaultValueConverter;
+	}
+    }
+
+    protected static mkKey(itemType: string, occurrenceIndicator: string): string {
+	return itemType + occurrenceIndicator;
+    }
+
+    /**
+     * Removes all registered converters for default values.
+     */
+    static resetDefaultValueConverters(): void {
+	XSFormFieldFactory.defaultValueConverters = {};
+    }
+
+}
+
+/**
+ * Register all the converters from this package.
+ */
+export function registerDefaultValueConverters(): void {
+    XSFormFieldFactory.addDefaultValueConverter("xs:string", "", new StringDefaultValueConverter());
+    XSFormFieldFactory.addDefaultValueConverter("xs:boolean", "", new BooleanDefaultValueConverter());
+    XSFormFieldFactory.addDefaultValueConverter("all", "", new PassDefaultValueConverter());
 }
