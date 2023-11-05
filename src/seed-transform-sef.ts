ceb648d1c206e2ad06f6290ce067cbd076fd2e1c
Christian Lück <christian.lueck@uni-muenster.de>
2023-11-05 13:29:04 +0100

    use workaround, pass parameters to SaxonJS, documentation
    This is related to #9

--- src/seed-transform-sef.ts
+++ src/seed-transform-sef.ts
@@ -1,22 +1,30 @@
 import { PropertyValues } from 'lit'
-import { customElement } from 'lit/decorators.js'
-import { SaxonJS } from 'saxon-js'; //const SaxonJS = require('saxon-js');
+import { customElement, property } from 'lit/decorators.js'
+
+//import { SaxonJS } from 'saxon-js'; // broken, see https://saxonica.plan.io/issues/6233
+import { SaxonJSLike } from './saxon-js-workaround.ts'
 
 import { SeedTransformer } from './transformation-api-client.ts'
 
 
 /**
- * The <seed-transform-rest> web component performs a transformation
- * by sending a request to the TrensformerAPI implemented by the SEED
- * XML Transformer.
+ * The `<seed-transform-sef>` web component performs an XSLT
+ * transformation inside the browser via SaxonJS.
  *
  * The component is designed to get its relevant input for making the
  * request from elements containing it, be it a dynamic for choosing a
- * transformation like <seed-choose-transform-rest> or a more complex
+ * transformation like `<seed-choose-transform-rest>` or a more complex
  * web application like a digital edition's web site.
  */
 @customElement("seed-transform-sef")
 export class SeedTransformSEF extends SeedTransformer {
+
+    /**
+     * A transformation engine, e.g., SaxonJS, has to be passed in to
+     * make this component work.
+     */
+    @property()
+    public engine: SaxonJSLike | undefined;
 
     /**
      * In the willUpdate() hook we watch out for changes of the
@@ -26,6 +34,7 @@ export class SeedTransformSEF extends SeedTransformer {
      */
     override async willUpdate(changedProperties: PropertyValues<this>) {
 	console.log("will update called");
+	console.log("SaxonJSLike transformer", this.engine);
 	if (this.transformation != null &&
 	    (changedProperties.has("transformation") ||
 		changedProperties.has("parameters") ||
@@ -42,11 +51,12 @@ export class SeedTransformSEF extends SeedTransformer {
 		console.log("from URL, POST parameters");
 		try {
 		    const content = ""; // TODO: get file from URL
-		    const result = await SaxonJS.transform({
+		    const result = await this.engine?.transform({
 			stylesheetLocation: this.getStylesheetLocation(),
 			sourceType: "xml",
 			sourceText: content,
 			sourceBaseURI: this.href,
+			stylesheetParams: this.parameters,
 			destination: "serialized"});
 		    this._result = result?.principalResult;
 		    this._mediaType = "text/xml"; // TODO: response?.headers?.["content-type"];
@@ -66,11 +76,12 @@ export class SeedTransformSEF extends SeedTransformer {
 		    }
 
 		    const content = await this.src.text();
-		    const result = await SaxonJS.transform({
+		    const result = await this.engine?.transform({
 			stylesheetLocation: this.getStylesheetLocation(),
 			sourceType: "xml",
 			sourceText: content,
 			sourceBaseURI: systemId,
+			stylesheetParams: this.parameters,
 			destination: "serialized"});
 		    this._result = result?.principalResult;
 		    this._mediaType = "text/xml"; // response?.headers?.["content-type"];
@@ -88,7 +99,7 @@ export class SeedTransformSEF extends SeedTransformer {
     }
 
     getStylesheetLocation(): string {
-	return this.transformationApi + "transform/" + this.transformation + "/stylesheet.sef";
+	return this.transformationApi + "transform/" + this.transformation + "/transformation.sef";
     }
 }
 
