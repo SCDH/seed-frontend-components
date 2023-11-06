287cf559cfdb4bceb7415cac18c0206f5e3f66e6
Christian Lück <christian.lueck@uni-muenster.de>
2023-11-06 01:51:38 +0100

    added main.ts as central import/export file

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { SeedSynopsisSyncComponent, IContentMeta } from './isynopsis'
+export type { SeedSynopsisSyncComponent, IContentMeta }
+import { SeedTextViewElement, SeedTypedTextViewElement } from './itextview'
+export type { SeedTextViewElement, SeedTypedTextViewElement }
+import { SaxonJSLike } from './saxon-js-workaround'
+export type { SaxonJSLike }
+import { SeedDownloadLink } from './seed-download-link'
+export type { SeedDownloadLink }
+import { SeedSynopsisText } from './seed-synopsis-text'
+export type { SeedSynopsisText }
+import { SeedSynopsis } from './seed-synopsis'
+export type { SeedSynopsis }
+import { SeedChooseTransformREST, SeedTransformRestParams } from './seed-transform-forms'
+export type { SeedChooseTransformREST, SeedTransformRestParams }
+import { SeedTransformREST } from './seed-transform-rest'
+export type { SeedTransformREST }
+import { SeedTransformSEF } from './seed-transform-sef'
+export type { SeedTransformSEF }
+import { WorkaroundApi } from './workaround-transformer-api'
+export type { WorkaroundApi }
+import { DefaultValueConverter, XSFormFieldFactory } from './xsform'
+import { StringDefaultValueConverter, BooleanDefaultValueConverter, PassDefaultValueConverter } from './xsform'
+export type { DefaultValueConverter, XSFormFieldFactory }
+export type { StringDefaultValueConverter, BooleanDefaultValueConverter, PassDefaultValueConverter }
