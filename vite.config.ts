2735dee64662b572c58ac7d69d24caea76f43c78
Christian Lück <christian.lueck@uni-muenster.de>
2023-11-28 00:49:45 +0100

    build artifact is one js file

--- vite.config.ts
+++ vite.config.ts
@@ -8,28 +8,14 @@ export default defineConfig(({ command, mode }) => {
   return {
 	build: {
 	    lib: {
-		entry: [resolve(__dirname, "src/isynopsis.ts"),
-			resolve(__dirname, "src/seed-synopsis.ts"),
-			resolve(__dirname, "src/seed-synopsis-text.ts"),
-			resolve(__dirname, "src/seed-transform-rest.ts"),
-			resolve(__dirname, "src/seed-transform-sef.ts"),
-			resolve(__dirname, "src/seed-transform-forms.ts"),
-			resolve(__dirname, "src/transformation-api-client.ts"),
-			resolve(__dirname, "src/xsform.ts"),
-			resolve(__dirname, "src/workaround-transformer-api.ts"),
-			resolve(__dirname, "src/saxon-js-workaround.ts"),
-			resolve(__dirname, "src/itextview.ts"),
-			resolve(__dirname, "src/seed-download-link.ts"),
-			resolve(__dirname, "src/seed-synopsis-text.ts")
-		       ],
-		//entry: resolve(__dirname, "./src/main.ts"),
+		entry: resolve(__dirname, "./src/main.ts"),
 		name: "SeedFrontendComponents",
 		fileName: "seed-frontend-components",
 		formats: ["es", "cjs"],
 	    },
 	    rollupOptions: {
-	    // 	external: mode === "production" ? "" : /^lit/,
-	    },
+		// 	external: mode === "production" ? "" : /^lit/,
+	    }
 	},
   }
 });
