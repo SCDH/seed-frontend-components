8b5230098262209cbe3297531d45f83a381e8227
Christian Lück <christian.lueck@uni-muenster.de>
2024-03-07 17:11:24 +0100

    add the text slice to the redux state store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import textsReducer from "./textsSlice";
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+	texts: textsReducer,
+    },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
