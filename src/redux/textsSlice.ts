1f854f6f3375e4322b656082d539060be6fd8b53
Christian Lück <christian.lueck@uni-muenster.de>
2024-03-12 13:32:03 +0100

    added more properties to TextSate, added initTextWidget action

--- src/redux/textsSlice.ts
+++ src/redux/textsSlice.ts
@@ -9,7 +9,32 @@ export interface TextState {
      * The position (id of a fragment) which the text is actually
      * scrolled to.
      */
-    scrollPosition: string;
+    scrollPosition: string | null;
+
+    /*
+     * The origin URL, obtained by `window.location.origin`.
+     */
+    origin: string | null;
+
+    /*
+     * The href, obtained by `window.location.href`.
+     */
+    href: string | null;
+
+    /*
+     * The pathname, obtained by `window.location.pathname`.
+     */
+    pathname: string | null;
+
+    /*
+     * The canonical URL has present in the header.
+     */
+    canonicalUrl: string | null;
+
+    /*
+     * The title has present in the header.
+     */
+    title: string | null;
 }
 
 /*
@@ -39,15 +64,43 @@ const textsSlice = createSlice({
     name: "texts",
     initialState,
     reducers: {
-	addText: (state, action: PayloadAction<{id: string, text: TextState}>) => {
-	    state[action.payload.id] = action.payload.text;
+	/*
+	 * The {initTextWidget} action adds a text widget of type
+	 * {TextState} to the slice. The properties of {TextState} are
+	 * all `null`. This action can be called early in the
+	 * initialization process, to make sure, that a property with
+	 * the ID of the text widget is present in the slice.
+	 */
+	initTextWidget: (state, action: PayloadAction<{id: string}>) => {
+	    state[action.payload.id] = {
+		scrollPosition: null,
+		origin: null,
+		href: null,
+		pathname: null,
+		canonicalUrl: null,
+		title: null };
 	},
+	/*
+	 * The {addText} action adds meta data of a text to the
+	 * slice's property for a text widget.
+	 */
+	addText: (state, action: PayloadAction<{id: string, text: TextState}>) => {
+	    state[action.payload.id] = {
+		...action.payload.text,
+		// The scroll position is to be left untouched. For
+		// the {scrolledTo} action might have be called before.
+		scrollPosition: state[action.payload.id].scrollPosition
+	    };
+	},
+	/*
+	 * The {scrolledTo} action updates the scroll position of a text widget.
+	 */
 	scrolledTo: (state, action: PayloadAction<{id: string, position: string}>) => {
 	    state[action.payload.id].scrollPosition = action.payload.position;
 	},
     },
 });
 
-export const { addText, scrolledTo } = textsSlice.actions;
+export const { initTextWidget, addText, scrolledTo } = textsSlice.actions;
 
 export default textsSlice.reducer;
