54065bb87298be44714edf8ab2d487b097957494
Christian Lück <christian.lueck@uni-muenster.de>
2024-03-12 17:40:59 +0100

    fetch json with annotations per segment via async thunk

--- src/redux/segmentsSlice.ts
+++ src/redux/segmentsSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
 
+import { TextState, TextsSlice } from "./textsSlice";
 
 /*
  * A segments state is a mapping of segment IDs to annotation IDs for
@@ -21,20 +22,36 @@ export interface SegmentsSlice {
 
 const initialState: SegmentsSlice = {}
 
+/*
+ * A helper interface to access the {TextsSlice} of the redux store.
+ */
+interface StoreHelper {
+    texts: TextsSlice,
+}
+
 
 /*
  * An async action for getting the SegmentState for a text widget's
  * from the backend.
  */
-export const getAnnotationsPerSegment = createAsyncThunk(
+export const getAnnotationsPerSegment = createAsyncThunk<any, string, { state: StoreHelper }>(
     "segments/getAnnotationsPerSegment",
-    async (textWidgetId: string): Promise<{textWidgetId: string, segments: SegmentsState}> => {
-	await new Promise((resolve) => setTimeout(resolve, 1000));
-	var segs: SegmentsState = {}; // TODO
-	return { textWidgetId: textWidgetId, segments: segs };
+    async (textWidgetId_, { getState }): Promise<{textWidgetId: string, segments: SegmentsState}> => {
+	const textSlice: TextsSlice = getState().texts;
+	const textState: TextState | null = textSlice[textWidgetId_];
+	const docHref: string = textState?.href ?? "";
+	const segmentsHref: string = docHref + ".segments.json";
+	console.log("fetching annotated segments from ", segmentsHref);
+	const response = await fetch(segmentsHref);
+	return response.json().then((result) => {
+	    return {textWidgetId: textWidgetId_, segments: result};
+	}).catch(() => {
+	    // If fetching failed, then let's have the empty mapping
+	    // of segment IDs to annotations IDs:
+	    return {textWidgetId: textWidgetId_, segments: {} };
+	});
     }
 )
-
 
 const segmentsSlice = createSlice({
     name: "segments",
@@ -42,14 +59,14 @@ const segmentsSlice = createSlice({
     reducers: {
     },
     extraReducers: (builder) => {
-	// when using addCase, the type parameter of the promise
+	// Note: When using addCase, the type parameter of the promise
 	// returned by the async thunk and the type paramter of the
 	// payload action must be the same.
 	builder.addCase(
 	    getAnnotationsPerSegment.fulfilled,
 	    (state, action: PayloadAction<{textWidgetId: string, segments: SegmentsState}>) => {
 		state[action.payload.textWidgetId] = action.payload.segments;
-	});
+	    });
     },
 });
 
