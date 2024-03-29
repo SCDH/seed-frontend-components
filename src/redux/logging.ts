b84bb43de7fceda424717b8983c640677354056a
Christian Lück <christian.lueck@uni-muenster.de>
2024-03-29 11:48:17 +0100

    provide logger for the redux part of the library

---
+++ src/redux/logging.ts
@@ -0,0 +1,9 @@
+import anylogger from "anylogger";
+
+/*
+ * We are using a logging facade throughout the library. Thus, the app
+ * developper can use whatever logging implementation she wants.
+ *
+ * See https://www.npmjs.com/package/anylogger
+ */
+export const log = anylogger("seed-frontend-components/redux");
