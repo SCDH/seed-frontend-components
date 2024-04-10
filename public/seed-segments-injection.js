c9c45b0ed720d3b62dde72670e0ce5f26f8a0c72
Christian Lück <christian.lueck@uni-muenster.de>
2024-04-10 22:30:54 +0200

    make code less redundant, improved readability

--- public/seed-segments-injection.js
+++ public/seed-segments-injection.js
@@ -1,34 +1,22 @@
-// add event listener for mouse events on segments with ID: post messages on these events
-function postMessageOnSegmentEvent() {
-    console.log("add event listener for mouse over and mouse out events: post messages on these events");
-    document.body.addEventListener("mouseover", function(e) {
-	e = e || window.event;
-	var targetElement = e.target || e.srcElement;
-	if (targetElement.hasAttribute('id')) {
-	    var idsInSubtree = seedAncestorOrSelfIds(targetElement, []);
-	    parent.postMessage({ ...msg, "event": "mouse-over-segment", "segmentId": targetElement.id, "subtreeIds": idsInSubtree }, window.location.href);
-	}
-    });
-    document.body.addEventListener("mouseout", function(e) {
-	e = e || window.event;
-	var targetElement = e.target || e.srcElement;
-	if (targetElement.hasAttribute('id')) {
-	    var idsInSubtree = seedAncestorOrSelfIds(targetElement, []);
-	    parent.postMessage({ ...msg, "event": "mouse-out-segment", "segmentId": targetElement.id, "subtreeIds": idsInSubtree }, window.location.href);
-	}
-    });
-    document.body.addEventListener("click", function(e) {
-	e = e || window.event;
-	var targetElement = e.target || e.srcElement;
-	if (targetElement.hasAttribute('id')) {
-	    var idsInSubtree = seedAncestorOrSelfIds(targetElement, []);
-	    parent.postMessage({ ...msg, "event": "click-segment", "segmentId": targetElement.id, "subtreeIds": idsInSubtree }, window.location.href);
-	}
-    });
+// add event listener for mouse events on segments/spans
+function seedRegisterOnSegmentEvents() {
+    console.debug("add event listener for mouse over and mouse out events");
+    document.body.addEventListener("mouseover", seedPostMessageOnSegmentEvent("mouse-over-segment"));
+    document.body.addEventListener("mouseout", seedPostMessageOnSegmentEvent("mouse-out-segment"));
+    document.body.addEventListener("click", seedPostMessageOnSegmentEvent("click-segment"));
 }
 window.addEventListener("load", (event) => {
-    postMessageOnSegmentEvent();
+    seedRegisterOnSegmentEvents();
 });
+// this returns the function to be fired on the event
+const seedPostMessageOnSegmentEvent = (eventType) => {
+    return function(e) {
+	e = e || window.event;
+	var targetElement = e.target || e.srcElement;
+	var ids = seedAncestorOrSelfIds(targetElement, []);
+	parent.postMessage({ ...msg, "event": eventType, "segmentIds": ids }, window.location.href);
+    };
+}
 /*
  * Recursively collects the IDs from the element and its
  * ancestors. The IDs from ancestors are of interest, since
