51740b46c5dba50ef66f5172273bcb81f6d696d2
Christian Lück <christian.lueck@uni-muenster.de>
2024-05-12 03:19:03 +0200

    allow loading of synopsis alignments

--- src/seed-synopsis.ts
+++ src/seed-synopsis.ts
@@ -1,20 +1,40 @@
-import { html, css, LitElement, CSSResult, TemplateResult } from 'lit'
+import { html, css, LitElement, CSSResult, TemplateResult, PropertyValues } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 
 import { widgetSizeProvider } from './widget-size-provider'
+import { storeConsumerMixin } from './store-consumer-mixin'
+import { fetchRegexAlignment, fetchMappingAlignment } from './redux/synopsisSlice'
+
+
 
 // define the web component
 @customElement("seed-synopsis")
-export class SeedSynopsis extends widgetSizeProvider(LitElement) {
+export class SeedSynopsis extends widgetSizeProvider(storeConsumerMixin(LitElement)) {
 
     @property({ type: String })
     id!: string;
+
+    @property({ attribute: "regex-alignment" })
+    regexAlignment!: string;
+
+    @property({ attribute: "mapping-alignment" })
+    mappingAlignment!: string;
 
     @property({ type: String, reflect: true })
     width: string = "100%";
 
     @property({ type: String, reflect: true })
     height: string = "100%";
+
+    protected willUpdate(changedProperties: PropertyValues<this>): void {
+	if (changedProperties.has("regexAlignment") && this.regexAlignment) {
+	    this.store?.dispatch(fetchRegexAlignment(this.regexAlignment));
+	}
+	if (changedProperties.has("mappingAlignment") && this.mappingAlignment) {
+	    this.store?.dispatch(fetchMappingAlignment(this.mappingAlignment));
+	}
+	super.willUpdate(changedProperties);
+    }
 
     protected styleTemplate(): TemplateResult<1> {
 	return html`<style>:host { display: block; width: ${this.width}; height: ${this.height} } div.synopsis { width: 100%; height: 100% }</style>`;
