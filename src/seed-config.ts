44544aee56e74b3585677bead2dbe90e2b45dbee
Christian Lück <christian.lueck@uni-muenster.de>
2024-05-14 00:26:48 +0200

    get all central data via seed-config element: annotations, ontology

--- src/seed-config.ts
+++ src/seed-config.ts
@@ -1,14 +1,20 @@
-import { LitElement, html, PropertyValues } from 'lit';
+import { LitElement, PropertyValues, html } from 'lit';
 import { property } from 'lit/decorators.js';
 import { customElement } from 'lit/decorators.js'
 
 import { storeConsumerMixin } from './store-consumer-mixin';
 import { fetchMappingAlignment, fetchRegexAlignment } from './redux/synopsisSlice';
+import { fetchAnnotations } from './redux/annotationsSlice';
+import { fetchResourceCenteredJson } from './redux/ontologySlice';
 
 @customElement('seed-config')
 export class SeedConfig extends storeConsumerMixin(LitElement) {
 
-    // _provider = new ContextProvider(this, {context: seedStoreContext, initialValue: store});
+    @property({ attribute: "ontology-urls", type: String })
+    ontologyUrls!: string;
+
+    @property({ attribute: "annotations-url", type: String })
+    annotationsUrl!: string;
 
     @property({ attribute: "regex-alignment" })
     regexAlignment!: string;
@@ -17,10 +23,16 @@ export class SeedConfig extends storeConsumerMixin(LitElement) {
     mappingAlignment!: string;
 
     protected willUpdate(changedProperties: PropertyValues<this>): void {
-	if (changedProperties.has("regexAlignment") && this.regexAlignment) {
+	if (changedProperties.has("annotationsUrl" as keyof SeedConfig)) {
+	    this.store?.dispatch(fetchAnnotations(this.annotationsUrl));
+	}
+	if (changedProperties.has("ontologyUrls" as keyof SeedConfig)) {
+	    this.store?.dispatch(fetchResourceCenteredJson(this.ontologyUrls));
+	}
+	if (changedProperties.has("regexAlignment" as keyof SeedConfig) && this.regexAlignment) {
 	    this.store?.dispatch(fetchRegexAlignment(this.regexAlignment));
 	}
-	if (changedProperties.has("mappingAlignment") && this.mappingAlignment) {
+	if (changedProperties.has("mappingAlignment" as keyof SeedConfig) && this.mappingAlignment) {
 	    this.store?.dispatch(fetchMappingAlignment(this.mappingAlignment));
 	}
 	super.willUpdate(changedProperties);
