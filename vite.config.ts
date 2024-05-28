2012ffeb07b74bfbd1bf8c941c0c9388f66e99c8
Christian Lück <christian.lueck@uni-muenster.de>
2024-05-28 18:43:29 +0200

    vite config for making demo pages from the examples

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,27 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  return {
+    if (mode === 'examples') {
+	return {
+	    base: "tei-processing/seed-frontend-components",
+	    build: {
+		outDir: "dist_examples",
+		assetsDir: "examples",
+		rollupOptions: {
+		    // 	external: mode === "production" ? "" : /^lit/,
+		    input: {
+			// add examples
+			examples: resolve(__dirname, 'index.html'),
+			synopsis: resolve(__dirname, 'examples/synopsis.html'),
+			figures: resolve(__dirname, 'examples/figures.html'),
+		    }
+		}
+	    },
+	    publicDir: false, // do not copy public
+	    assetsInclude: ["examples/*.json"],
+	}
+    }
+    return {
 	build: {
 	    lib: {
 		entry: resolve(__dirname, "./src/main.ts"),
