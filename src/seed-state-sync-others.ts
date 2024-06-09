761d54541bf4950956688bfe6e3d3a51a64aa122
Christian Lück <christian.lueck@uni-muenster.de>
2024-06-10 00:44:32 +0200

    get position from store

--- src/seed-state-sync-others.ts
+++ src/seed-state-sync-others.ts
@@ -1,9 +1,12 @@
 import { html, LitElement, CSSResultGroup, css } from 'lit'
 import { customElement } from 'lit/decorators.js'
 import { consume } from '@lit/context';
+import { addListener, UnknownAction } from '@reduxjs/toolkit';
 
 import { scrolledTextViewThunk } from './redux/synopsisActions';
 import { syncOthers } from './redux/synopsisSlice';
+import { TextViewsSlice } from "./redux/textViewsSlice";
+import { SeedState } from './redux/seed-store';
 
 import log from "./logging";
 import { SeedSynopsisText } from './seed-synopsis-text';
@@ -22,6 +25,26 @@ export class SeedStateSyncOthers extends storeConsumerMixin(LitElement) {
     @consume({ context: seedTextViewContext })
     protected textView!: SeedSynopsisText;
 
+    position?: string;
+
+    subscribeStore(): void {
+	if (this.store === undefined) {
+	    log.debug("no store yet");
+	}
+	this.store?.dispatch(addListener({
+	    predicate: (_action: UnknownAction, currentState, _previousState): boolean => {
+		// log.debug("checking predicate for element with Id " + this.id);
+		let currState: { textViews: TextViewsSlice } = currentState as SeedState;
+		return currState.textViews.hasOwnProperty(this.textView?.id ?? "_") && currState.textViews[this.textView?.id ?? "_"]?.scrollPosition !== this.position;
+	    },
+	    effect: (_action, listenerApi): void => {
+		//log.debug("cssPerSegment updated for element with Id " + this.id)
+		let state: SeedState = listenerApi.getState() as SeedState;
+		this.position = state.textViews[this.textView?.id ?? "_"]?.scrollPosition ?? undefined;
+	    }
+	}));
+    }
+
     render() {
 	return html`<span class="window-status-item">
 	    <button class="unicode-button" @click="${this.syncOtherViews}" title="Sync others!">&#x2194;</botton>
@@ -30,7 +53,9 @@ export class SeedStateSyncOthers extends storeConsumerMixin(LitElement) {
 
     protected syncOtherViews = (_e: Event) => {
 	log.debug("syncing others");
-	this.store?.dispatch(scrolledTextViewThunk(syncOthers, this.textView.id, [this.textView.position]));
+	if (this.position) {
+	    this.store?.dispatch(scrolledTextViewThunk(syncOthers, this.textView.id, [this.position]));
+	}
     }
 
     // see https://lit.dev/docs/components/styles/#inheriting-styles-from-a-superclass
