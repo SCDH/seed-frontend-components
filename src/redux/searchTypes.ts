df4e8aa8103b5c602e0040d6b568db96d9b731b6
Christian Lück <christian.lueck@uni-muenster.de>
2025-02-21 19:07:01 +0100

    types for searching with Solr

---
+++ src/redux/searchTypes.ts
@@ -0,0 +1,118 @@
+/*
+ * The type for a Solr response data object returned on `/solr/COLLECTION/select?...`
+ */
+export interface SearchResponse {
+
+    responseHeader: ResponseHeader,
+
+    response: Response
+
+}
+
+/*
+ * The type of the header in the Solr response data object.
+ */
+export interface ResponseHeader {
+
+    zkConnected: boolean,
+    status: bigint,
+    QTime: bigint,
+    params: Parameters
+
+}
+
+export interface Parameters {
+
+    q: string,
+    indent: boolean,
+    useParams: string,
+    _: string
+
+}
+
+/*
+ * The type of the "response" field in the Solr response data object.
+ */
+export interface Response {
+
+    numFound: number,
+    start: number,
+    numFoundExact: boolean,
+    docs: Array<Document>,
+    facet_counts: undefined | FacetCounts
+
+}
+
+/*
+ * A document is an object, where the property names are names of
+ * stored Solr fields and the property values are stored fields. So
+ * the structure depends on the indexed data and the managed schema.
+ */
+export interface Document {
+
+    [field: string]: FieldValue
+}
+
+/*
+ * The field values are either arrays or singlular field values.
+ */
+export type FieldValue = Array<any> | any;
+
+/*
+ * The facets part of the Solr response object.
+ */
+export interface FacetCounts {
+
+    facet_queries: any,
+    facet_fields: FacetFields,
+    facet_ranges: any,
+    facet_intervals: any,
+    facet_heatmaps: any
+
+}
+
+/*
+ * The facet fields are similar to `Document`, but the value is an
+ * array of field values interleaved with counts. Ugh, ugly!
+ */
+export interface FacetFields {
+
+    [facet_field: string]: Array<any | Number>
+
+}
+
+
+export const initialResponseHeader = {
+    zkConnected: false,
+    status: 0,
+    QTime: 0,
+    params: {}
+}
+
+export const initialResponse: Response = {
+    numFound: 0,
+    start: 0,
+    numFoundExact: true,
+    docs: [],
+    facet_counts: undefined,
+}
+
+export const initialSearchResponse = {
+    responseHeader: initialResponseHeader,
+    response: initialResponse
+}
+
+
+
+/*
+ * The parameters of a search query are stored in an extra slice.
+ *
+ * TODO: same as Parameters + collection
+ */
+export interface SearchQuery {
+
+    collection: string,
+
+    q: string,
+
+}
