ef18ae1f7beafe8421020f6b23e6619911dda577
Christian Lück <christian.lueck@uni-muenster.de>
2025-03-02 18:46:57 +0100

    use an update function to do all the updates

--- src/seed-search-result.ts
+++ src/seed-search-result.ts
@@ -2,7 +2,7 @@ import { html, LitElement } from 'lit'
 import { customElement, state } from 'lit/decorators.js'
 
 import { storeConsumerMixin } from './store-consumer-mixin';
-import { addAppListener } from "./redux/seed-store";
+import { addAppListener, SeedListenerApi } from "./redux/seed-store";
 import { searchApi } from "./redux/searchSlice";
 import { addFilter, removeFilter } from './redux/searchQuerySlice';
 import { SearchResponse, Document, solrSearchQuery } from "./redux/searchTypes";
@@ -59,45 +59,29 @@ export class SeedSearchResult extends storeConsumerMixin(LitElement) {
 	// 1. filter removed, result already present from document(...) query
 	this.store?.dispatch(addAppListener({
 	    matcher: removeFilter.match,
-	    effect: (_action, listenerApi) => {
-		const queryId: string = "documents(\"" + solrSearchQuery(listenerApi.getState().searchQuery).replaceAll("\"", "\\\"") + "\")";
-		log.debug("filter removed", queryId, listenerApi.getState().searchApi.queries.hasOwnProperty(queryId));
-		const data: SearchResponse | undefined = listenerApi.getState().searchApi.queries[queryId]?.data as SearchResponse | undefined;
-		if (data !== undefined) {
-		    this.document_count = data.response.numFound;
-		    this.document_start = data.response.start;
-		    this.documents = data.response.docs;
-		}
-	    }
+	    effect: (_action, listenerApi) => this.updateEffect(searchApi.endpoints.documents.name, listenerApi),
 	}));
 	// 2. filter removed, result already present from filter(...) query
 	this.store?.dispatch(addAppListener({
 	    matcher: removeFilter.match,
-	    effect: (_action, listenerApi) => {
-		const queryId: string = "filter(\"" + solrSearchQuery(listenerApi.getState().searchQuery).replaceAll("\"", "\\\"") + "\")";
-		log.debug("filter removed", queryId, listenerApi.getState().searchApi.queries.hasOwnProperty(queryId));
-		const data: SearchResponse | undefined = listenerApi.getState().searchApi.queries[queryId]?.data as SearchResponse | undefined;
-		if (data !== undefined) {
-		    this.document_count = data.response.numFound;
-		    this.document_start = data.response.start;
-		    this.documents = data.response.docs;
-		}
-	    }
+	    effect: (_action, listenerApi) => this.updateEffect(searchApi.endpoints.filter.name, listenerApi),
 	}));
 	// 2. filter added, result already present from filter(...) query
 	this.store?.dispatch(addAppListener({
 	    matcher: addFilter.match,
-	    effect: (_action, listenerApi) => {
-		const queryId: string = "filter(\"" + solrSearchQuery(listenerApi.getState().searchQuery).replaceAll("\"", "\\\"") + "\")";
-		log.debug("filter removed", queryId, listenerApi.getState().searchApi.queries.hasOwnProperty(queryId));
-		const data: SearchResponse | undefined = listenerApi.getState().searchApi.queries[queryId]?.data as SearchResponse | undefined;
-		if (data !== undefined) {
-		    this.document_count = data.response.numFound;
-		    this.document_start = data.response.start;
-		    this.documents = data.response.docs;
-		}
-	    }
+	    effect: (_action, listenerApi) => this.updateEffect(searchApi.endpoints.filter.name, listenerApi),
 	}));
+    }
+
+    updateEffect(endpoint: string, listenerApi: SeedListenerApi): void {
+	const queryId: string = endpoint + "(\"" + solrSearchQuery(listenerApi.getState().searchQuery).replaceAll("\"", "\\\"") + "\")";
+	log.debug("filter removed", queryId, listenerApi.getState().searchApi.queries.hasOwnProperty(queryId));
+	const data: SearchResponse | undefined = listenerApi.getState().searchApi.queries[queryId]?.data as SearchResponse | undefined;
+	if (data !== undefined) {
+	    this.document_count = data.response.numFound;
+	    this.document_start = data.response.start;
+	    this.documents = data.response.docs;
+	}
     }
 
     render() {
