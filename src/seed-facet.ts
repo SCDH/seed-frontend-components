641c4fee87b814232919eebf7723bcafc4b0f58f
Christian Lück <christian.lueck@uni-muenster.de>
2025-03-03 15:13:34 +0100

    use seed-facet-term web component for rendering terms

--- src/seed-facet.ts
+++ src/seed-facet.ts
@@ -4,7 +4,7 @@ import { customElement, property, state } from 'lit/decorators.js'
 import { storeConsumerMixin } from './store-consumer-mixin';
 import { addAppListener } from "./redux/seed-store";
 import { searchApi } from './redux/searchSlice';
-import { addFilter, removeFilter } from './redux/searchQuerySlice';
+//import { addFilter, removeFilter } from './redux/searchQuerySlice';
 import { FacetTerms, SearchResponse, toTermCountTuples, TermCountTuple, solrSearchQuery } from './redux/searchTypes';
 
 import log from "./logging";
@@ -13,6 +13,9 @@ import log from "./logging";
 
 /*
  * The `seed-facet` web component renders a search facet.
+ *
+ * If this component is used, the seed-facet-term component
+ * has to be loaded, too.
  */
 @customElement("seed-facet")
 export class SeedFacet extends storeConsumerMixin(LitElement) {
@@ -55,34 +58,7 @@ export class SeedFacet extends storeConsumerMixin(LitElement) {
     }
 
     renderTerm(t: TermCountTuple) {
-	return html`<div class="term">
-	    <input type="checkbox" id="${t.term}" name="${this.field}" value="${t.term}" @change="${this.changed(t.term)}">
-	    <label for="${t.term}">${t.term}</label>
-	    <span>${t.count}</span>
-	</div>`;
-    }
-
-    changed = (term: string) => {
-	return ((e: Event) => {
-	    const origin: HTMLInputElement = e.composedPath()[0] as HTMLInputElement;
-	    const checked: boolean = origin.checked ?? false;
-	    log.debug("facet term changed: ", term, checked, this);
-	    if (checked) {
-		this.store?.dispatch(addFilter({field: this.field, term: term }));
-		const s = this.store?.getState().searchQuery;
-		log.debug("search query", s);
-		if (s !== undefined) {
-		    this.store?.dispatch(searchApi.endpoints.filter.initiate(s));
-		}
-	    } else {
-		this.store?.dispatch(removeFilter({field: this.field, term: term }));
-		const s = this.store?.getState().searchQuery;
-		log.debug("search query", s);
-		if (s !== undefined) {
-		    this.store?.dispatch(searchApi.endpoints.filter.initiate(s));
-		}
-	    }
-	});
+	return html`<seed-facet-term field="${this.field}" term="${t.term}" count="${t.count}"></seed-facet-term>`;
     }
 
     static styles: CSSResultGroup = [
