1ce73f32a548e1fd259a58c9ba05cc61e008f97b
Christian Lück <christian.lueck@uni-muenster.de>
2025-03-03 23:08:00 +0100

    add fields to search response

--- src/seed-search-result.ts
+++ src/seed-search-result.ts
@@ -1,9 +1,11 @@
-import { html, LitElement } from 'lit'
-import { customElement, state } from 'lit/decorators.js'
+import { html, LitElement, HTMLTemplateResult } from 'lit'
+import { customElement, state, property } from 'lit/decorators.js'
 
 import { searchResultMixin } from './search-result-mixin';
-import { SeedListenerApi } from "./redux/seed-store";
+import { SeedListenerApi, addAppListener } from "./redux/seed-store";
+import { searchApi } from './redux/searchSlice';
 import { SearchResponse, Document, solrSearchQuery } from "./redux/searchTypes";
+import { addFl } from './redux/searchQuerySlice';
 
 import log from "./logging";
 
@@ -14,6 +16,12 @@ import log from "./logging";
 @customElement("seed-search-result")
 export class SeedSearchResult extends searchResultMixin(LitElement) {
 
+    @property()
+    collection!: string;
+
+    @property()
+    pattern: string = "^(meta|author|title)";
+
     @state()
     documents: Array<Document> = [];
 
@@ -23,6 +31,21 @@ export class SeedSearchResult extends searchResultMixin(LitElement) {
     @state()
     document_start: number = 0;
 
+    override subscribeStore(): void {
+	// add fields to be included in the response, by adding them to the query parameter fl
+	this.store?.dispatch(addAppListener({
+	    matcher: searchApi.endpoints.fields.matchFulfilled,
+	    effect: (_action, listenerApi) => {
+		const queryId: string = searchApi.endpoints.fields.name + "(\"" + this.collection + "\")";
+		const flds: Array<string> = listenerApi.getState().searchApi?.queries?.[queryId]?.data as Array<string> ?? [];
+		const regex: RegExp = new RegExp(this.pattern);
+		const fl: Array<string> = flds.filter(f => f.match(regex));
+		log.debug("adding fields to query", queryId, fl);
+		listenerApi.dispatch(addFl(fl));
+		}
+	}));
+	super.subscribeStore();
+    }
 
     override updateEffect(endpoint: string, listenerApi: SeedListenerApi): void {
 	const queryId: string = endpoint + "(\"" + solrSearchQuery(listenerApi.getState().searchQuery).replaceAll("\"", "\\\"") + "\")";
@@ -35,14 +58,21 @@ export class SeedSearchResult extends searchResultMixin(LitElement) {
 	}
     }
 
-    override render() {
+    override render(): HTMLTemplateResult {
 	return html`<div>
-                ${this.renderDocumentCount()}
+	    ${this.renderDocumentCount()}
+	    <div class="result-documents">
+		${this.documents.map(d => this.renderDocument(d, this.pattern))}
+	    </div>
             </div>`;
     }
 
-    renderDocumentCount() {
+    renderDocumentCount(): HTMLTemplateResult {
 	return html`<div><span>Documents found:</span><span>${this.document_count}</span></div>`;
+    }
+
+    renderDocument(doc: Document, pattern: string): HTMLTemplateResult {
+	return html`<seed-result-doc doc-id="${doc.id}" document="${doc}" pattern="${pattern}"></seed-result-doc>`;
     }
 
 }
