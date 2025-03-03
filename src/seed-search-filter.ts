5e94ab9f28aea3900a7e5d4019a423821949a398
Christian Lück <christian.lueck@uni-muenster.de>
2025-03-03 12:14:55 +0100

    web components for search filter overview and deletion of filters

---
+++ src/seed-search-filter.ts
@@ -0,0 +1,47 @@
+import { html, LitElement, HTMLTemplateResult } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+
+import { storeConsumerMixin } from './store-consumer-mixin';
+import { removeFilter } from './redux/searchQuerySlice';
+import { searchApi } from './redux/searchSlice';
+
+
+/*
+ * A web component for a removable search filter displayed in a filter status view.
+ */
+@customElement("seed-search-filter")
+export class SeedSearchFilter extends storeConsumerMixin(LitElement) {
+
+    @property()
+    field!: string;
+
+    @property()
+    term!: string;
+
+    override render(): HTMLTemplateResult {
+	return html`<button class="removable-filter" type="button" @click="${this.rmFilter}" title="remove this filter">
+	    <span class="remove-filter-symbol dispose">&#x1F5D9;</span>
+	    <span>${this.field}</span><span class="field-term-sep">:</span
+	    <span>${this.term}</span>
+	</button>`;
+    }
+
+    rmFilter():void {
+	this.store?.dispatch(removeFilter({field: this.field, term: this.term}));
+	// The filter endpoint has to be initiated, because the new
+	// combination of filters may not have been queried before.
+	// If no filters are applied any more, calling the documents
+	// endpoint is not required. Is it?
+	if (Object.keys(this.store?.getState()?.searchQuery?._fq_faceted ?? {}).length > 0) {
+	    this.store?.dispatch(searchApi.endpoints.filter.initiate(this.store?.getState()?.searchQuery));
+	}
+    }
+
+}
+
+
+declare global {
+    interface HTMLElementTagNameMap {
+	"seed-search-filter": SeedSearchFilter;
+    }
+}
---
+++ src/seed-search-filters.ts
@@ -0,0 +1,71 @@
+import { html, LitElement, HTMLTemplateResult } from 'lit'
+import { customElement, state } from 'lit/decorators.js'
+
+import { storeConsumerMixin } from './store-consumer-mixin';
+import { addAppListener } from './redux/seed-store';
+import { addFilter, removeFilter } from './redux/searchQuerySlice';
+
+import log from "./logging";
+
+
+/*
+ * A web component that shows which search filters are applied.
+ *
+ * If this component is used, `seed-search-filter` (singular!)
+ * must be used, too.
+ */
+@customElement("seed-search-filters")
+export class SeedSearchFilters extends storeConsumerMixin(LitElement) {
+
+    @state()
+    filters: Array<{field: string, term: string}> = [];
+
+    @state()
+    count: number = 0;
+
+    override subscribeStore(): void {
+	// filter added
+	this.store?.dispatch(addAppListener({
+	    matcher: addFilter.match,
+	    effect: (action, _listenerApi) => {
+		log.debug("search filter added", action.payload);
+		this.filters.push(JSON.parse(JSON.stringify(action.payload))); // deep copy
+		this.count++;
+	    },
+	}));
+	// filter removed
+	this.store?.dispatch(addAppListener({
+	    matcher: removeFilter.match,
+	    effect: (action, _listenerApi) => {
+		log.debug("search filter removed", action.payload);
+		this.filters = this.filters.filter(f => !(f.field == action.payload.field && f.term == action.payload.term));
+		this.count--;
+	    }
+	}));
+    }
+
+    override render(): HTMLTemplateResult {
+	log.debug("rendering search filters:", this.filters);
+	return html`<div class="search-filters">
+	    <div class="title">
+                <slot name="title">Search filters (${this.count})</slot>
+	    </div>
+	    <div class="filters">
+		${this.filters.map(this.renderFilter)}
+	    </div>
+        </div>`;
+    }
+
+    renderFilter(filter: {field: string, term: string}): HTMLTemplateResult {
+	log.debug("search filter", filter);
+	return html`<seed-search-filter field="${filter.field}" term="${filter.term}"}></seed-search-filter>`;
+    }
+
+}
+
+
+declare global {
+    interface HTMLElementTagNameMap {
+	"seed-search-filters": SeedSearchFilters;
+    }
+}
