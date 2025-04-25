9fa608c959b2c79a726b14c57883c901f42bd4cc
Christian Lück <christian.lueck@uni-muenster.de>
2025-04-25 11:58:28 +0200

    multiple entry points

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,23 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
+    if (mode === "redux") {
+        return {
+            build: {
+                outDir: "./dist/redux",
+                lib: {
+                    entry: resolve(__dirname, "./src/redux/main.ts"),
+                    name: "SEED Frontend State",
+                    fileName: "main",
+                    formats: ["es"],
+                },
+                rollupOptions: {
+                    // 	external: mode === "production" ? "" : /^lit/,
+                },
+            },
+            plugins: [dts({ include: ["src/redux"] })],
+        };
+    }
     return {
         build: {
             lib: {
