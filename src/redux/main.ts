a8b7f44ac3303bb1738c8ba953779b28e228b371
Christian Lück <christian.lueck@uni-muenster.de>
2025-05-01 15:25:50 +0200

    export components of router slice

--- src/redux/main.ts
+++ src/redux/main.ts
@@ -1,3 +1,10 @@
 export { store } from "./store";
 export type { RootState, AppDispatch } from "./store";
 export { fetchDataLabels } from "./dataLabelSlice";
+export type {
+    Route,
+    RouterSlice,
+    ParamValue,
+    PrimitiveParamValue,
+} from "./routerSlice";
+export { to } from "./routerSlice";
