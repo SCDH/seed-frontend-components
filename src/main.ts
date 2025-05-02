5fa3debb15d2c0fcb927aef02669907f6cbd5d2c
Christian Lück <christian.lueck@uni-muenster.de>
2025-05-02 09:23:02 +0200

    introduce <seed-result-details-link> web component

--- src/main.ts
+++ src/main.ts
@@ -31,3 +31,4 @@ export { SeedFacetTerm } from "./seed-facet-term.ts";
 export { SeedDataLabel } from "./seed-data-label.ts";
 export { SeedSearchFilter } from "./seed-search-filter.ts";
 export { SeedSearchFilters } from "./seed-search-filters.ts";
+export { SeedResultDetailsLink } from "./seed-result-details-link.ts";
---
+++ src/seed-result-details-link.ts
@@ -0,0 +1,32 @@
+import { LitElement, HTMLTemplateResult, html } from "lit";
+import { customElement, property } from "lit/decorators.js";
+
+@customElement("seed-result-details-link")
+export class SeedResultDetailsLink extends LitElement {
+    @property()
+    collection!: string;
+
+    @property({ attribute: "doc-id" })
+    documentId!: string;
+
+    /*
+     * Path segment to search, used for in the path to details view.
+     */
+    @property({ attribute: "search-path" })
+    searchPath: string = "/search/";
+
+    protected override render(): HTMLTemplateResult {
+        console.info("rendering link");
+        return html`<a
+            href="${this.searchPath}${this.collection}/detail/${this
+                .documentId}"
+            >${this.documentId}</a
+        >`;
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        "seed-result-details-link": SeedResultDetailsLink;
+    }
+}
