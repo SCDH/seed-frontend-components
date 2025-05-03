03d5307427528983e1fb9ce6fe053c5bf5c82d13
Christian Lück <christian.lueck@uni-muenster.de>
2025-05-03 18:39:13 +0200

    export search API

--- src/redux/main.ts
+++ src/redux/main.ts
@@ -1,6 +1,10 @@
 export { store } from "./store";
 export type { RootState, AppDispatch } from "./store";
+export { searchApi } from "./searchSlice";
+export type { SearchState } from "./searchSlice";
 export { fetchDataLabels } from "./dataLabelSlice";
+export * from "./searchTypes";
+export * from "./searchQuerySlice";
 export type {
     Route,
     RouterSlice,
