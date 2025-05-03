63f6ce6be50408a4d872db6009613402568c7974
Christian Lück <christian.lueck@uni-muenster.de>
2025-05-03 17:44:21 +0200

    use decorator

--- src/seed-result-details.ts
+++ src/seed-result-details.ts
@@ -1,10 +1,15 @@
-import { HTMLTemplateResult, html } from "lit";
+import { HTMLTemplateResult, html, PropertyValues } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 
-import { SeedState, addAppListener } from "./redux/seed-store";
+import { SeedState } from "./redux/seed-store";
 import { searchApi } from "./redux/searchSlice";
 import { StoreConsumerElement } from "./store-consumer-mixin";
-import { addSingleDocFilter, setCollection } from "./redux/searchQuerySlice";
+import { matched } from "./store-consumer-decorators";
+import {
+    addSingleDocFilter,
+    removeSingleDocFilter,
+    setCollection,
+} from "./redux/searchQuerySlice";
 import {
     SearchResponse,
     Document,
@@ -37,47 +42,37 @@ export class SeedResultDetails extends StoreConsumerElement<SeedState, any> {
     @property({ attribute: "doc-id" })
     documentId!: string;
 
-    @state()
+    @matched<SeedState, SeedResultDetails, SearchResponse | undefined>(
+        searchApi.endpoints.document.matchFulfilled,
+        (s, _c) => {
+            const q: SearchQuery = s.searchQuery;
+            const queryId: string =
+                searchApi.endpoints.document.name +
+                '("' +
+                solrSearchQuery(q).replaceAll('"', '\\"') +
+                '")';
+            return s.searchApi.queries[queryId]?.data as
+                | SearchResponse
+                | undefined;
+        },
+    )
     result!: SearchResponse;
 
     @state()
     document!: Document;
 
-    subscribeStore(): void {
-        // add listener to the store, that reports when the query is fulfilled
-        this.store?.dispatch(
-            addAppListener({
-                matcher: searchApi.endpoints.document.matchFulfilled,
-                effect: (_action, listenerApi) => {
-                    // In the effect, the query at the time when the
-                    // match occurs is used, not the query at the time
-                    // of subscribing the listener. This allows us to
-                    // update this costum element with other
-                    // documents. We just have to make a new query.
-                    const q: SearchQuery = listenerApi.getState().searchQuery;
-                    const queryId: string =
-                        'document("' +
-                        solrSearchQuery(q).replaceAll('"', '\\"') +
-                        '")';
-                    log.debug(
-                        "retrieved single document",
-                        queryId,
-                        listenerApi
-                            .getState()
-                            .searchApi.queries.hasOwnProperty(queryId),
-                    );
-                    this.result = listenerApi.getState().searchApi.queries[
-                        queryId
-                    ]?.data as SearchResponse;
-                    this.document = this.result.response.docs[0];
-                },
-            }),
-        );
-        // query the document
-        this.query();
+    override disconnectedCallback(): void {
+        // When the element is removed from the dom, the single
+        // document filter must be removed from the search query slice.
+        this.store?.dispatch(removeSingleDocFilter());
+        super.disconnectedCallback();
     }
 
+    /*
+     * Initiates a request for the document given by ID in the `documentId` property.
+     */
     protected query() {
+        log.debug("initiate request for document", this.documentId);
         // set up query
         this.store?.dispatch(setCollection(this.collection));
         this.store?.dispatch(addSingleDocFilter(this.documentId));
@@ -86,6 +81,22 @@ export class SeedResultDetails extends StoreConsumerElement<SeedState, any> {
             this.store?.getState()?.searchQuery ?? initialSearchQuery;
         // initiate this query
         this.store?.dispatch(searchApi.endpoints.document.initiate(qry));
+    }
+
+    protected override willUpdate(
+        changedProperties: PropertyValues<this>,
+    ): void {
+        super.willUpdate(changedProperties);
+        // When the result comes in, also set the `document` property
+        // from the result.
+        if (changedProperties.has("result")) {
+            this.document = this.result.response.docs[0];
+        }
+        // When the documentId property is updated, a new request is
+        // send.
+        if (changedProperties.has("documentId")) {
+            this.query();
+        }
     }
 
     protected override render(): HTMLTemplateResult {
