8a77c0c2a561593c51ce8f7ea1598eb496c0d173
Christian Lück <christian.lueck@uni-muenster.de>
2025-05-28 08:18:26 +0200

    meta data fields and full text field per properties

--- src/seed-result-details.ts
+++ src/seed-result-details.ts
@@ -1,11 +1,12 @@
 import { HTMLTemplateResult, html, PropertyValues } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
+import { UnsubscribeListener } from "@reduxjs/toolkit";
 import { StoreConsumerElement } from "@scdh/lit-redux-consumer";
 import { matched } from "@scdh/lit-redux-consumer";
 
 import { SeedState } from "./redux/seed-store";
 import { searchApi } from "./redux/searchSlice";
-import { setCollection } from "./redux/searchQuerySlice";
+import { setCollection, setQueryFields } from "./redux/searchQuerySlice";
 import {
     SearchResponse,
     Document,
@@ -38,15 +39,26 @@ export class SeedResultDetails extends StoreConsumerElement<SeedState, any> {
     @property({ attribute: "doc-id" })
     documentId!: string;
 
+    @property({ attribute: "field-pattern" })
+    fieldPattern: string = "^(meta|author|title)";
+
+    @property({ attribute: "text-pattern" })
+    textPattern: string = "^(html_htm_)";
+
     @matched<SeedState, SeedResultDetails, SearchResponse | undefined>(
         searchApi.endpoints.document.matchFulfilled,
-        (s, _c) => {
+        (s, c) => {
             const q: SearchQuery = s.searchQuery;
             const queryId: string =
                 searchApi.endpoints.document.name +
                 '("' +
-                solrSearchQuery(q) +
+                solrSearchQuery(q, c?.documentId ?? undefined) +
                 '")';
+            log.debug(
+                "search result loaded",
+                s.searchApi.queries[queryId],
+                queryId,
+            );
             return s.searchApi.queries[queryId]?.data as
                 | SearchResponse
                 | undefined;
@@ -57,12 +69,56 @@ export class SeedResultDetails extends StoreConsumerElement<SeedState, any> {
     @state()
     document!: Document;
 
+    @state()
+    highlighting!: Document | undefined;
+
     // override disconnectedCallback(): void {
     //     // When the element is removed from the dom, the single
     //     // document filter must be removed from the search query slice.
     //     this.store?.dispatch(removeSingleDocFilter());
     //     super.disconnectedCallback();
     // }
+
+    protected override subscribeStore(): void {
+        if (this.store === undefined) {
+            log.error("no store yet for element", this);
+            return;
+        }
+        this.setFields(this.store.getState());
+        // If the field name in the search index were already
+        // requested, take them from the redux store and set up the
+        // fields property.
+        if (
+            this.store
+                .getState()
+                .searchApi.queries.hasOwnProperty(this.fieldsQueryId()) &&
+            this.store.getState().searchApi.queries[this.fieldsQueryId()]
+                ?.status == "fulfilled"
+        ) {
+            log.debug("fields alread queried when setting up details view");
+            this.setFields(this.store?.getState());
+            this.query();
+        } else {
+            // If not already in the request, initiate a request and
+            // set up a listener, that sets the fields property.
+            log.debug("initiating fields query", this.collection);
+            this.store?.dispatch(
+                searchApi.endpoints.fields.initiate(this.collection),
+            );
+            const unsubscriber = this.store?.dispatch(
+                this.addAppListener({
+                    matcher: searchApi.endpoints.fields.matchFulfilled,
+                    effect: async (_action, listenerApi) => {
+                        this.setFields(listenerApi.getState());
+                        this.query();
+                    },
+                }),
+            );
+            this._unsubscribers.push(
+                unsubscriber as unknown as UnsubscribeListener,
+            );
+        }
+    }
 
     /*
      * Initiates a request for the document given by ID in the `documentId` property.
@@ -84,6 +140,23 @@ export class SeedResultDetails extends StoreConsumerElement<SeedState, any> {
         );
     }
 
+    private setFields(state: SeedState): void {
+        const flds: Array<string> =
+            (state.searchApi?.queries?.[this.fieldsQueryId()]
+                ?.data as Array<string>) ?? [];
+        const fldRegex: RegExp = new RegExp(this.fieldPattern);
+        const txtRegex: RegExp = new RegExp(this.textPattern);
+        const qf: Array<string> = flds.filter(
+            (f) => f.match(fldRegex) || f.match(txtRegex),
+        );
+        log.debug("setting query fields for details view", qf);
+        this.store?.dispatch(setQueryFields(qf));
+    }
+
+    private fieldsQueryId(): string {
+        return searchApi.endpoints.fields.name + '("' + this.collection + '")';
+    }
+
     protected override willUpdate(
         changedProperties: PropertyValues<this>,
     ): void {
@@ -91,21 +164,32 @@ export class SeedResultDetails extends StoreConsumerElement<SeedState, any> {
         // When the result comes in, also set the `document` property
         // from the result.
         if (changedProperties.has("result")) {
+            log.debug("result was updated");
             this.document = this.result.response.docs[0];
-        }
-        // When the documentId property is updated, a new request is
-        // send.
-        if (changedProperties.has("documentId")) {
-            this.query();
+            this.highlighting = this.result.highlighting
+                ? [this.documentId]
+                : undefined;
         }
     }
 
     protected override render(): HTMLTemplateResult {
+        log.error("result", this.result);
         if (this.result?.response?.numFound != 1) {
-            return html`Getting document with ID ${this.documentId} ...`;
+            return html`Getting document with ID ${this.documentId} ...
+            ${this.result}`;
         }
         return html`<div>
-            ${this.result?.response?.numFound ?? "failed"} ${this.document.id}
+            <div>
+                ${this.result?.response?.numFound ?? "failed"}
+                ${this.document.id}
+            </div>
+            <seed-result-doc
+                collection="${this.collection}"
+                doc-id="${this.documentId}"
+                .document="${this.document}"
+                .highlight="${this.highlighting}"
+                pattern="${this.fieldPattern}"
+            ></seed-result-doc>
         </div>`;
     }
 }
